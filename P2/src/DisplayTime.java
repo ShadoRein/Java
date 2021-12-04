1b7e32b0dfb0f46d88b601660630ad6a02cdb5ea
ShadoRein <91312451+ShadoRein@users.noreply.github.com>
2021-12-05 00:09:44 +0800

    Add files via upload

---
+++ P2/src/DisplayTime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DisplayTime {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		//Promote the user for input
+		System.out.print("Enter an integer for seconds: ");
+		int seconds = input.nextInt();
+		int minutes = seconds / 60;//Find minutes in seconds
+		int remainingSeconds = seconds % 60;//Seconds remaining
+		System.out.println(seconds + " seconds is " + minutes +
+		" minutes and " + remainingSeconds + " seconds");
+		
+	}
+
+}
---
+++ P2/src/ShowRuntimeErrors.java
@@ -0,0 +1,9 @@
+
+public class ShowRuntimeErrors {
+
+	public static void main(String[] args) {
+		System.out.println(0/1);
+
+	}
+
+}
